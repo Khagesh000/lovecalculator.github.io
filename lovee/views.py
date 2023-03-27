3cbe4b70ff54b4ac1541af4472909a5bd3930929
Khagesh000 <128826992+Khagesh000@users.noreply.github.com>
2023-03-27 19:21:48 +0530

    First commit

---
+++ lovee/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+
+    if request.method == 'POST':
+    
+        data = str(request.POST.get('num1'))
+        data1 = str(request.POST.get('num2'))
+        value = 0
+        value1 = 0
+        value2 = 0
+        r = 1
+        rev = 0
+        k = 0
+        j = 0
+        result = 0
+
+        for char in data:
+            value += ord(char)
+
+        for char1 in data1:
+            value1 += ord(char1)
+
+        r = value % 10
+        last = r
+
+        while(value!=0):
+            r = value%10
+            rev = rev+r
+            k += r
+            value = value/10
+
+        p = value1%10
+        late = p
+
+        while(value1!=0):
+            p = value1%10
+            rev = rev+p
+            j += p
+            value1 = value1/10
+
+            
+        sum = last+k+j+late+33
+        result = int(sum)
+
+        return render(request, 'index.html', {'result':result})
+
+    else:
+        return render(request, "index.html")
+
+
---
+++ static/index.js
@@ -0,0 +1,54 @@
+function fn1(){
+    var name = document.getElementById("num").value;
+    var name1 = document.getElementById("num1").value;
+    // var getx = /^[0-9]$/
+    var getx1 = /^[a-zA-Z.]+$/;
+    var getx2 = /^[a-zA-Z.]+$/;
+
+    if(name == ""){
+        document.getElementById("demo").innerHTML="Must be filled";
+        return false;
+    }
+   else if(name.trim() == ""){
+        document.getElementById("demo").innerHTML="Blanks are not allowed";
+        return false;
+    }
+    else if(name.length<=2){
+        document.getElementById("demo").innerHTML="Invalid username";
+        return false;
+    }
+    else if(!name.match(getx1)){
+        document.getElementById("demo").innerHTML="Numbers are not allowed";
+        return false;
+    }
+   
+    else if(name.match(getx1)){
+        document.getElementById("demo").innerHTML="";
+    
+    }
+
+        
+
+    if(name1 == ""){
+        document.getElementById("demo1").innerHTML="Must be filled";
+        return false;
+        
+    }
+    else if(name1.trim() == ""){
+        document.getElementById("demo1").innerHTML="Blanks are not allowed";
+        return false;
+    }
+    else if(name1.length<=2){
+        document.getElementById("demo1").innerHTML="Invalid username";
+        return false;
+    }
+    else if(!name1.match(getx2)){
+        document.getElementById("demo1").innerHTML="Numbers are not allowed";
+        return false;
+    }
+    else if(name1.match(getx2)){
+        document.getElementById("demo1").innerHTML="";
+        return true;
+    }
+}
+   
